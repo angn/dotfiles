3c4ed77e5473106e56cae5cab7953c2d77306d85
holies <para.keon@gmail.com>
2013-01-08 00:27:19 -0800

    Add Sublime Text 2 settings

---
+++ sublime.js
@@ -0,0 +1,7 @@
+{
+	"color_scheme": "Packages/Color Scheme - Default/Twilight.tmTheme",
+	"default_line_ending": "unix",
+	"font_size": 11,
+	"rulers": [78, 80]
+}
+# https://raw.github.com/icrowley/st2_packages/master/Missing%20Palette%20Commands/Missing.sublime-commands
