456eb714e642504a17a17cd9b499cc1d4f31bf13
keon <para.keon@gmail.com>
2025-05-14 12:37:42 +0900

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -eux
+ln -s ~/conf/gitconfig ~/.gitconfig
+ln -s ~/conf/my.cnf ~/.my.cnf
+ln -s ~/conf/tmux.conf ~/.tmux.conf
+ln -s ~/conf/vimrc ~/.vimrc
+ln -s ~/conf/zimrc ~/.zimrc
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
