ba514b6bbf3ae8d4327e4fc0dbcefaf749d6b375
keon <para.keon@gmail.com>
2025-05-27 13:21:22 +0900

    conf -> dotfiles

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -eux
-ln -s ~/conf/gitignore ~/.gitignore
-ln -s ~/conf/gitconfig ~/.gitconfig
-ln -s ~/conf/my.cnf ~/.my.cnf
-ln -s ~/conf/tmux.conf ~/.tmux.conf
-ln -s ~/conf/vimrc ~/.vimrc
-ln -s ~/conf/zimrc ~/.zimrc
+ln -sf ~/dotfiles/gitignore ~/.gitignore
+ln -sf ~/dotfiles/gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/my.cnf ~/.my.cnf
+ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/vimrc ~/.vimrc
+ln -sf ~/dotfiles/zimrc ~/.zimrc
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
