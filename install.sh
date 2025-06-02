484976ccd4850a2c3f1d8c34266f61c1d0d3f6bb
keon <para.keon@gmail.com>
2025-06-02 10:48:35 +0900

    Add aliases

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ ln -sf ~/dotfiles/vimrc ~/.vimrc
 ln -sf ~/dotfiles/zimrc ~/.zimrc
 ln -sf ~/dotfiles/tmux ~/.local/bin/tmux
 ln -sf ~/dotfiles/kmux ~/.local/bin/kmux
+echo 'source ~/dotfiles/aliases' >> ~/.zshrc
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
