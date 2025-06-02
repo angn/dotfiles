77eedbf70d26d6390e62a2743f5a9586e74c2e88
keon <para.keon@gmail.com>
2025-06-02 10:45:28 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
-#!/bin/bash -eux
 ln -sf ~/dotfiles/gitignore ~/.gitignore
 ln -sf ~/dotfiles/gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/my.cnf ~/.my.cnf
 ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/vimrc ~/.vimrc
 ln -sf ~/dotfiles/zimrc ~/.zimrc
+ln -sf ~/dotfiles/tmux ~/.local/bin/tmux
+ln -sf ~/dotfiles/kmux ~/.local/bin/kmux
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
