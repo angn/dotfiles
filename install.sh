2d1762fa49404ac01dc64e762f877539a3cd95c4
keon <para.keon@gmail.com>
2025-06-05 23:56:25 +0900

    Update tssh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ ln -sf ~/dotfiles/my.cnf ~/.my.cnf
 ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/vimrc ~/.vimrc
 ln -sf ~/dotfiles/zimrc ~/.zimrc
-ln -sf ~/dotfiles/tmux ~/.local/bin/tmux
+ln -sf ~/dotfiles/tssh ~/.local/bin/tssh
 ln -sf ~/dotfiles/kmux ~/.local/bin/kmux
 echo 'source ~/dotfiles/aliases' >> ~/.zshrc
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
