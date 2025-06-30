d118c48b6469753652bfe02e3472e75844c34b5c
keon <para.keon@gmail.com>
2025-06-30 10:40:48 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,15 @@
-# brew
-# kube-ps1
-# mise
-ln -sf ~/dotfiles/gitignore ~/.gitignore
-ln -sf ~/dotfiles/gitconfig ~/.gitconfig
-ln -sf ~/dotfiles/my.cnf ~/.my.cnf
-ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
-ln -sf ~/dotfiles/vimrc ~/.vimrc
-ln -sf ~/dotfiles/zimrc ~/.zimrc
-ln -sf ~/dotfiles/tssh ~/.local/bin/tssh
-ln -sf ~/dotfiles/kmux ~/.local/bin/kmux
-echo 'source ~/dotfiles/aliases' >> ~/.zshrc
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+#!/bin/bash -eux
+ln -vsnf ~/dotfiles/gitignore ~/.gitignore
+ln -vsnf ~/dotfiles/gitconfig ~/.gitconfig
+ln -vsnf ~/dotfiles/my.cnf ~/.my.cnf
+ln -vsnf ~/dotfiles/tmux.conf ~/.tmux.conf
+ln -vsnf ~/dotfiles/vimrc ~/.vimrc
+ln -vsnf ~/dotfiles/zimrc ~/.zimrc
+ln -vsnf ~/dotfiles/tssh ~/.local/bin/tssh
+ln -vsnf ~/dotfiles/kmux ~/.local/bin/kmux
+grep -qxF 'source ~/dotfiles/aliases' ~/.zshrc || echo 'source ~/dotfiles/aliases' >> ~/.zshrc
+zimfw --version || curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
+curl -fsSLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+brew --version || curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash
+brew install mise kube-ps1
 mise cfg set settings.idiomatic_version_file_enable_tools --type=list node,ruby
