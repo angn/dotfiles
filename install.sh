cce2a6920b8d1305891388f6160d3485a1ed06f6
keon <para.keon@gmail.com>
2026-06-08 10:24:43 +0900

    Update zshrc

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
-#!/bin/bash -eux
+#!/bin/zsh -eux
+
 ln -vsnf ~/dotfiles/gitignore ~/.gitignore
 ln -vsnf ~/dotfiles/gitconfig ~/.gitconfig
 ln -vsnf ~/dotfiles/my.cnf ~/.my.cnf
@@ -7,10 +8,25 @@ ln -vsnf ~/dotfiles/vimrc ~/.vimrc
 ln -vsnf ~/dotfiles/zimrc ~/.zimrc
 ln -vsnf ~/dotfiles/tssh ~/.local/bin/tssh
 ln -vsnf ~/dotfiles/kmux ~/.local/bin/kmux
-grep -qxF 'source ~/dotfiles/aliases' ~/.zshrc || echo 'source ~/dotfiles/aliases' >> ~/.zshrc
-zimfw --version || curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
-curl -fsSLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-brew --version || curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash
-brew install mise kube-ps1
+
+if [ ! -f ~/.zim/zimfw.zsh ]; then
+	curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
+fi
+
+if [ ! -d ~/.local/share/fast-syntax-highlighting ]; then
+	git clone --depth=1 --single-branch https://github.com/zdharma-continuum/fast-syntax-highlighting ~/.local/share/fast-syntax-highlighting
+fi
+
+if [ ! -d ~/kube-ps1 ]; then
+	git clone --depth=1 --single-branch https://github.com/jonmosco/kube-ps1 ~/kube-ps1
+fi
+
+if [ "$(uname -s)" = Darwin ]; then
+	brew --version || curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash
+fi
+
+mise --version || curl -fsS https://mise.run | sh
 mise cfg set settings.idiomatic_version_file_enable_tools --type=list node,ruby
 mise use --global fzf rg k9s stern
+
+curl -fsSLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
--- zshrc
+++ zshrc
@@ -1,7 +1,8 @@
-if [ ! -f ~/.local/share/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh ]; then
-	git clone --depth=1 --single-branch https://github.com/zdharma-continuum/fast-syntax-highlighting ~/.local/share/fast-syntax-highlighting
-fi
 source ~/.local/share/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
+
+if [ -x /opt/homebrew/bin/brew ]; then
+	source <(/opt/homebrew/bin/brew shellenv)
+fi
 
 source ~/kube-ps1/kube-ps1.sh
 KUBE_PS1_SYMBOL_ENABLE=false
@@ -15,4 +16,23 @@ stern() {
 
 source <(fzf --zsh)
 
-source ~/dotfiles/aliases
+alias activate='source .venv/bin/activate'
+
+if command -v nerdctl &> /dev/null; then
+	alias docker=nerdctl
+fi
+
+alias kdk='kubectl diff -k'
+alias kak='kubectl apply -k'
+alias kgew='kubectl get events -w'
+alias kgnowide='kubectl get nodes -o wide'
+alias kgawide='kubectl get all -o wide'
+alias krr='kubectl rollout restart'
+alias krs='kubectl rollout status'
+alias krun='kubectl run -it --rm --restart=Never --image-pull-policy=IfNotPresent --image=curlimages/curl curl-$RANDOM-$$ -- sh'
+alias keit='kubectl exec -it'
+# alias kgponode='kubectl get po -o wide -A --field-selector spec.nodeName='
+
+if which explorer.exe 2> /dev/null >&2; then
+	alias open=explorer.exe
+fi
